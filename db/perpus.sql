5eed7ac6e323cb1678ea31f62d8312f68e338897
Muhammad Airlangga <43737135+mrlng@users.noreply.github.com>
2024-01-02 00:16:09 +0700

    update 2/1/2024

--- db/perpus.sql
+++ db/perpus.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 28 Des 2023 pada 20.18
+-- Waktu pembuatan: 01 Jan 2024 pada 18.11
 -- Versi server: 10.4.27-MariaDB
 -- Versi PHP: 8.1.12
 
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `anggota` (
-  `anggota_id` int(11) NOT NULL,
+  `anggota_id` char(8) NOT NULL,
   `nama` varchar(100) DEFAULT NULL,
   `alamat` varchar(200) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
@@ -40,7 +40,7 @@ CREATE TABLE `anggota` (
 --
 
 INSERT INTO `anggota` (`anggota_id`, `nama`, `alamat`, `email`, `telepon`) VALUES
-(1, 'Angga', 'Bangsri', 'angga@uninsu.ac.id', '0898243142');
+('1', 'Angga', 'Jerukwangi', 'angga@unisnu.ac.id', '085951612818');
 
 -- --------------------------------------------------------
 
@@ -49,12 +49,12 @@ INSERT INTO `anggota` (`anggota_id`, `nama`, `alamat`, `email`, `telepon`) VALUE
 --
 
 CREATE TABLE `buku` (
-  `buku_id` int(11) NOT NULL,
+  `buku_id` char(8) NOT NULL,
   `judul` varchar(100) DEFAULT NULL,
   `pengarang` varchar(100) DEFAULT NULL,
   `penerbit` varchar(100) DEFAULT NULL,
   `tahun_terbit` int(11) DEFAULT NULL,
-  `sinopsis` text NOT NULL,
+  `sinopsis` text DEFAULT NULL,
   `kategori_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -63,8 +63,9 @@ CREATE TABLE `buku` (
 --
 
 INSERT INTO `buku` (`buku_id`, `judul`, `pengarang`, `penerbit`, `tahun_terbit`, `sinopsis`, `kategori_id`) VALUES
-(1001, 'Cara Menjadi Angga', 'Angga', 'Airlangga', 2100, 'Lorem ipsum sit dolor amet', 1),
-(1002, 'History of Angga', 'Angga', 'Airalangga', 2050, 'Lorem ipsum sit dolor amet', 1);
+('B-1CMA', 'Cara Menjadi Angga', 'Angga', 'Airlangga', 2024, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec eleifend lorem. Cras blandit lectus gravida felis tincidunt, ac suscipit lacus malesuada. Vivamus urna enim, euismod quis dapibus et, dictum eu nisi. Proin eleifend iaculis sapien eget lacinia. Praesent sit amet metus nibh. Quisque felis enim, consequat sed ultricies at, tincidunt id elit. Pellentesque luctus, risus eget rhoncus porttitor, augue libero tincidunt tortor, eu faucibus mi enim eu libero. Duis eget mi non magna condimentum fringilla. Mauris eu purus scelerisque, maximus est bibendum, euismod odio. Duis porttitor ligula lectus. Sed vel metus at lectus faucibus varius.\r\n\r\n', 1),
+('B-1HOA', 'History of Angga', 'Angga', 'Airlangga', 2023, 'Sejarah sosok angga', 2),
+('B-1PMB', 'Pemrograman Mobile', 'Angga', 'Airlangga', 2020, 'tak da', 9);
 
 -- --------------------------------------------------------
 
@@ -73,18 +74,21 @@ INSERT INTO `buku` (`buku_id`, `judul`, `pengarang`, `penerbit`, `tahun_terbit`,
 --
 
 CREATE TABLE `katalog` (
-  `katalog_id` int(11) NOT NULL,
-  `buku_id` int(11) DEFAULT NULL,
-  `kategori_id` int(11) DEFAULT NULL
+  `katalog_id` char(8) NOT NULL,
+  `buku_id` char(8) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `katalog`
 --
 
-INSERT INTO `katalog` (`katalog_id`, `buku_id`, `kategori_id`) VALUES
-(2, 1001, 1),
-(3, 1002, 1);
+INSERT INTO `katalog` (`katalog_id`, `buku_id`) VALUES
+('B-1CMA0', 'B-1CMA'),
+('B-1CMA1', 'B-1CMA'),
+('B-1HOA2', 'B-1HOA'),
+('B-1PMB0', 'B-1PMB'),
+('B-1PMB1', 'B-1PMB'),
+('B-1PMB2', 'B-1PMB');
 
 -- --------------------------------------------------------
 
@@ -102,7 +106,9 @@ CREATE TABLE `kategori` (
 --
 
 INSERT INTO `kategori` (`kategori_id`, `nama_kategori`) VALUES
-(1, 'Fiksi');
+(1, 'Non Fiksi'),
+(2, 'Sejarah'),
+(9, 'Ilmiah');
 
 -- --------------------------------------------------------
 
@@ -112,8 +118,9 @@ INSERT INTO `kategori` (`kategori_id`, `nama_kategori`) VALUES
 
 CREATE TABLE `peminjaman` (
   `peminjaman_id` int(11) NOT NULL,
-  `buku_id` int(11) DEFAULT NULL,
-  `anggota_id` int(11) DEFAULT NULL,
+  `buku_id` char(8) NOT NULL,
+  `katalog_id` char(8) DEFAULT NULL,
+  `anggota_id` char(8) DEFAULT NULL,
   `tanggal_peminjaman` date DEFAULT NULL,
   `tanggal_kembali` date DEFAULT NULL,
   `status` enum('dipinjam','kembali') DEFAULT 'dipinjam'
@@ -123,8 +130,10 @@ CREATE TABLE `peminjaman` (
 -- Dumping data untuk tabel `peminjaman`
 --
 
-INSERT INTO `peminjaman` (`peminjaman_id`, `buku_id`, `anggota_id`, `tanggal_peminjaman`, `tanggal_kembali`, `status`) VALUES
-(3, 1002, 1, '2023-12-25', '2023-12-26', 'dipinjam');
+INSERT INTO `peminjaman` (`peminjaman_id`, `buku_id`, `katalog_id`, `anggota_id`, `tanggal_peminjaman`, `tanggal_kembali`, `status`) VALUES
+(5, 'B-1PMB', 'B-1PMB0', '1', '2024-01-01', '2024-01-01', 'kembali'),
+(6, 'B-1HOA', 'B-1HOA2', '1', '2024-01-01', '2024-01-01', 'kembali'),
+(7, 'B-1HOA', 'B-1HOA1', '1', '2024-01-01', '2024-01-01', 'dipinjam');
 
 -- --------------------------------------------------------
 
@@ -140,6 +149,14 @@ CREATE TABLE `pengembalian` (
   `status_pengembalian` enum('dikembalikan','terlambat') DEFAULT 'dikembalikan'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data untuk tabel `pengembalian`
+--
+
+INSERT INTO `pengembalian` (`pengembalian_id`, `peminjaman_id`, `tanggal_pengembalian`, `denda`, `status_pengembalian`) VALUES
+(3, 5, '2024-01-01', '0.00', 'dikembalikan'),
+(4, 6, '2024-01-01', '0.00', 'dikembalikan');
+
 -- --------------------------------------------------------
 
 --
@@ -147,12 +164,12 @@ CREATE TABLE `pengembalian` (
 --
 
 CREATE TABLE `staff` (
-  `staff_id` int(11) NOT NULL,
+  `staff_id` char(8) NOT NULL,
   `nama` varchar(100) DEFAULT NULL,
   `jabatan` varchar(100) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
   `telepon` varchar(20) DEFAULT NULL,
-  `sandi` varchar(25) NOT NULL
+  `sandi` char(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -160,7 +177,7 @@ CREATE TABLE `staff` (
 --
 
 INSERT INTO `staff` (`staff_id`, `nama`, `jabatan`, `email`, `telepon`, `sandi`) VALUES
-(1, 'Angga', 'staff', 'angga@unisnu.ac.id', '083956431233', 'admin');
+('admin', 'angga', 'ceo', 'angga@librarylte.com', '083956431233', 'admin');
 
 --
 -- Indexes for dumped tables
@@ -184,8 +201,7 @@ ALTER TABLE `buku`
 --
 ALTER TABLE `katalog`
   ADD PRIMARY KEY (`katalog_id`),
-  ADD KEY `buku_id` (`buku_id`),
-  ADD KEY `kategori_id` (`kategori_id`);
+  ADD KEY `buku_id` (`buku_id`);
 
 --
 -- Indeks untuk tabel `kategori`
@@ -198,8 +214,8 @@ ALTER TABLE `kategori`
 --
 ALTER TABLE `peminjaman`
   ADD PRIMARY KEY (`peminjaman_id`),
-  ADD KEY `buku_id` (`buku_id`),
-  ADD KEY `anggota_id` (`anggota_id`);
+  ADD KEY `anggota_id` (`anggota_id`),
+  ADD KEY `buku_id` (`buku_id`) USING BTREE;
 
 --
 -- Indeks untuk tabel `pengembalian`
@@ -219,46 +235,22 @@ ALTER TABLE `staff`
 --
 
 --
--- AUTO_INCREMENT untuk tabel `anggota`
---
-ALTER TABLE `anggota`
-  MODIFY `anggota_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT untuk tabel `buku`
---
-ALTER TABLE `buku`
-  MODIFY `buku_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1003;
-
---
--- AUTO_INCREMENT untuk tabel `katalog`
---
-ALTER TABLE `katalog`
-  MODIFY `katalog_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
 -- AUTO_INCREMENT untuk tabel `kategori`
 --
 ALTER TABLE `kategori`
-  MODIFY `kategori_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `kategori_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT untuk tabel `peminjaman`
 --
 ALTER TABLE `peminjaman`
-  MODIFY `peminjaman_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `peminjaman_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT untuk tabel `pengembalian`
 --
 ALTER TABLE `pengembalian`
-  MODIFY `pengembalian_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT untuk tabel `staff`
---
-ALTER TABLE `staff`
-  MODIFY `staff_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `pengembalian_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
@@ -268,27 +260,26 @@ ALTER TABLE `staff`
 -- Ketidakleluasaan untuk tabel `buku`
 --
 ALTER TABLE `buku`
-  ADD CONSTRAINT `buku_ibfk_1` FOREIGN KEY (`kategori_id`) REFERENCES `kategori` (`kategori_id`);
+  ADD CONSTRAINT `buku_ibfk_1` FOREIGN KEY (`kategori_id`) REFERENCES `kategori` (`kategori_id`) ON DELETE CASCADE ON UPDATE SET NULL;
 
 --
 -- Ketidakleluasaan untuk tabel `katalog`
 --
 ALTER TABLE `katalog`
-  ADD CONSTRAINT `katalog_ibfk_1` FOREIGN KEY (`buku_id`) REFERENCES `buku` (`buku_id`),
-  ADD CONSTRAINT `katalog_ibfk_2` FOREIGN KEY (`kategori_id`) REFERENCES `kategori` (`kategori_id`);
+  ADD CONSTRAINT `katalog_ibfk_1` FOREIGN KEY (`buku_id`) REFERENCES `buku` (`buku_id`);
 
 --
 -- Ketidakleluasaan untuk tabel `peminjaman`
 --
 ALTER TABLE `peminjaman`
-  ADD CONSTRAINT `peminjaman_ibfk_1` FOREIGN KEY (`buku_id`) REFERENCES `buku` (`buku_id`),
-  ADD CONSTRAINT `peminjaman_ibfk_2` FOREIGN KEY (`anggota_id`) REFERENCES `anggota` (`anggota_id`);
+  ADD CONSTRAINT `peminjaman_ibfk_2` FOREIGN KEY (`anggota_id`) REFERENCES `anggota` (`anggota_id`),
+  ADD CONSTRAINT `peminjaman_ibfk_3` FOREIGN KEY (`buku_id`) REFERENCES `buku` (`buku_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 --
 -- Ketidakleluasaan untuk tabel `pengembalian`
 --
 ALTER TABLE `pengembalian`
-  ADD CONSTRAINT `pengembalian_ibfk_1` FOREIGN KEY (`peminjaman_id`) REFERENCES `peminjaman` (`peminjaman_id`);
+  ADD CONSTRAINT `pengembalian_ibfk_1` FOREIGN KEY (`peminjaman_id`) REFERENCES `peminjaman` (`peminjaman_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
